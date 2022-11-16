1ae6727c928565bd3458d580ee66239f52d32111
amaliuts <ami24amalia@gmail.com>
2022-11-16 19:04:45 +0200

    setup

---
+++ BackEnd/TakeMeOut/TakeMeOut/Database/Models/BusinessAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TakeMeOut.Database.Models;
+
+public partial class BusinessAccount
+{
+    public int IdBa { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Email { get; set; }
+
+    public float? ContactNo { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; }
+
+    public int? IdVenue { get; set; }
+
+    public virtual ICollection<Event> Events { get; } = new List<Event>();
+
+    public virtual Venue? IdVenueNavigation { get; set; }
+}
---
+++ BackEnd/TakeMeOut/TakeMeOut/Database/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TakeMeOut.Database.Models;
+
+public partial class User
+{
+    public int IdUser { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? Email { get; set; }
+
+    public float? PhoneNo { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; }
+
+    public virtual ICollection<Event> Events { get; } = new List<Event>();
+
+    public virtual ICollection<Order> Orders { get; } = new List<Order>();
+
+    public virtual ICollection<Payment> Payments { get; } = new List<Payment>();
+
+    public virtual ICollection<Review> Reviews { get; } = new List<Review>();
+
+    public virtual ICollection<UserAction> UserActions { get; } = new List<UserAction>();
+}
