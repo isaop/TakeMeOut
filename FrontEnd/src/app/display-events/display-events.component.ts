213224d257dab1eab74ab84af16af054485eb0d7
Grig <andrewredean@gmail.com>
2022-11-18 16:41:09 +0200

    nu merge import la .js

--- FrontEnd/src/app/display-events/display-events.component.ts
+++ FrontEnd/src/app/display-events/display-events.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+// import "src/assets/display-events.js";
 
+
+// declare var display:any;
 @Component({
   selector: 'app-display-events',
   templateUrl: './display-events.component.html',
   styleUrls: ['./display-events.component.scss']
 })
+
+
+
 export class DisplayEventsComponent implements OnInit {
 
+  
+
+  // f(){
+  //   new display();
+  // }
   constructor() { }
 
   ngOnInit(): void {
