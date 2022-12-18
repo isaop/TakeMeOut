51e49b696073ebf77db098b88244dd752cb6ec27
amaliuts <ami24amalia@gmail.com>
2022-12-18 21:28:16 +0200

    added scope for venue & ba services

--- BackEnd/BackEnd/Program.cs
+++ BackEnd/BackEnd/Program.cs
@@ -7,7 +7,8 @@ using BackEnd.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IVenueService, VenueService>();
+builder.Services.AddScoped<IBusinessAccountService, BusinessAccountService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
