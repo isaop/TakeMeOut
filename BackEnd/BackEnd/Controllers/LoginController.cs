bddb43a008f8d22199860541b09fbd049f7fc17b
amaliuts <ami24amalia@gmail.com>
2022-12-19 19:52:59 +0200

    login for user - done

---
+++ BackEnd/BackEnd/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using BackEnd.Models;
+using BackEnd.Dtos;
+using BackEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserDtoLogin user)
+        {
+            if (user is null)
+                return BadRequest("Invalid client request");
+            var loginResponse = await _loginService.CreateTokenString(user);
+            return (loginResponse.GetType() == typeof(string)) ? Unauthorized(loginResponse) : Ok(loginResponse);
+        }
+    }
+}
