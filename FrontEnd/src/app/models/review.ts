9784abaaf28e2d6bedbccfa7cee4c0bbe33e5d01
AndradaNelus25 <andrada.nelus@yahoo.com>
2022-12-20 21:38:46 +0200

    createEvent and add review integration done

---
+++ FrontEnd/src/app/models/review.ts
@@ -0,0 +1,4 @@
+export interface Review {
+    score: number;
+    description: string;
+  }
--- FrontEnd/src/app/review/review.component.ts
+++ FrontEnd/src/app/review/review.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { Event } from '../models/event'
+import { Review } from '../models/review';
 
 @Component({
   selector: 'app-review',
@@ -7,16 +12,45 @@ import { Event } from '../models/event'
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent implements OnInit {
+
+  score = new FormControl('');
+  description = new FormControl('');
+
   event: Event = {
-    name: 'Event name',
+   name: 'Event name',
     description: 'quick yoga cred'
   };
-  constructor() { }
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
+  reviewForm = new FormGroup({
+    score: new FormControl(),
+    description: new FormControl(),
+  })
+
   PublishReview(){
-    //
+    let review : Review = {
+      score: this.reviewForm.get('score')?.value,
+      description: this.reviewForm.get('description')?.value,
+    }
+
+    this.addReview(review).subscribe((response) => {
+      console.log(response);
+      if(response.statusText == "OK")
+          this.router.navigate(['success']);
+    });
+  }
+
+  addReview(review: any){
+    return this.http.post(`${environment.BaseUrl}/Review/add-review`, review, {
+      observe: 'response',
+      responseType: 'text',
+    });
   }
 }
